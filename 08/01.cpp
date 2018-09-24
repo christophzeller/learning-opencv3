f6b8a5e7415196c15e6f6001ec7964654b7db46f
Zeller Christoph <chze7458@ESO9573-ubuntu.eso.local>
2018-09-24 14:25:32 +0200

    added slow solution for exercise 08.01.a

--- 08/01.cpp
+++ 08/01.cpp
@@ -25,14 +25,51 @@ slots.
 using std::cout; using std::endl;
 using namespace cv;
 
+const char* canny_window = "Canny";
+const char* raw_window = "Raw";
+const char* grayscale_window = "Grayscale";
+const char* composite_window = "Composite";
+
+struct Images
+{
+    Mat raw;
+    Mat grayscale;
+    Mat canny;
+};
+
+void show_separate(const Images& stuff)
+{
+    imshow(raw_window, stuff.raw);
+    imshow(grayscale_window, stuff.grayscale);
+    imshow(canny_window, stuff.canny);
+}
+
+void show_composite(Images& stuff)
+{
+    Mat composite_image ( stuff.raw.rows, stuff.raw.cols*3, stuff.raw.type() );
+    
+    cvtColor(stuff.grayscale, stuff.grayscale, COLOR_GRAY2BGR);
+    cvtColor(stuff.canny, stuff.canny, COLOR_GRAY2BGR);
+    
+    Mat roi_raw     ( composite_image, Rect { 0, 0, stuff.raw.cols, stuff.raw.rows} );
+    Mat roi_gs      { composite_image, Rect { stuff.raw.cols, 0, stuff.raw.cols, stuff.raw.rows} };
+    Mat roi_canny   { composite_image, Rect { stuff.raw.cols*2, 0, stuff.raw.cols, stuff.raw.rows} };
+    
+    stuff.raw.copyTo(roi_raw);
+    stuff.grayscale.copyTo(roi_gs);
+    stuff.canny.copyTo(roi_canny);
+    
+    imshow(composite_window, composite_image);
+
+}
 
 
 int main(int argc, char** argv)
 {
     // argv[1] == filename to video
-    if (argc != 2)
+    if (argc != 3)
     {
-        cout << "expects exactly one argument: path to video file" << endl;
+        cout << "expects exactly two argument: path to video file and mode [s|c]" << endl;
         return -1;
     }
     
@@ -43,13 +80,20 @@ int main(int argc, char** argv)
         cout << "could not open video file: " << argv[1] << endl;
     }
     
-    const char* canny_window = "Canny";
-    const char* raw_window = "Raw";
-    const char* grayscale_window = "Grayscale";
+
+    char mode = argv[2][0];
     
-    namedWindow(raw_window, WINDOW_AUTOSIZE);
-    namedWindow(grayscale_window, WINDOW_AUTOSIZE);
-    namedWindow(canny_window, WINDOW_AUTOSIZE);
+    switch (mode)
+    {
+    case 's':
+        namedWindow(raw_window, WINDOW_AUTOSIZE);
+        namedWindow(grayscale_window, WINDOW_AUTOSIZE);
+        namedWindow(canny_window, WINDOW_AUTOSIZE);
+    break;
+    case 'c':
+        namedWindow(composite_window, WINDOW_AUTOSIZE);
+    break;
+    }
     
     Mat frame;
     Mat grayscale;
@@ -58,15 +102,30 @@ int main(int argc, char** argv)
     {
         cvtColor(frame, grayscale, COLOR_BGR2GRAY); // TODO: dynamically determine COLOR_...2GRAY?
         Canny(grayscale, canny, 10, 100); // TODO: good threshold values?
+        
+        Images frame_data;
+        frame_data.raw = frame;
+        frame_data.grayscale = grayscale;
+        frame_data.canny = canny;
 
-        imshow(raw_window, frame);
-        imshow(grayscale_window, grayscale);
-        imshow(canny_window, canny);
+        switch (mode)
+        {
+        case 's': show_separate(frame_data); break;
+        case 'c': show_composite(frame_data); break;
+        }
         
         waitKey(20);        
     }
     
-    destroyWindow(raw_window);
-    destroyWindow(grayscale_window);
-    destroyWindow(canny_window);
+    switch (mode)
+    {
+    case 's':
+        destroyWindow(raw_window);
+        destroyWindow(grayscale_window);
+        destroyWindow(canny_window);
+        break;
+    case 'c':
+        destroyWindow(composite_window);
+        break;
+    }
 }
