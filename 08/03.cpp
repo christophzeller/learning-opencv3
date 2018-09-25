676414453581529d8a74a719274475ca4d64445c
Zeller Christoph <chze7458@ESO9573-ubuntu.eso.local>
2018-09-25 15:21:30 +0200

    added first draft of graphical histogram. needs some love.

--- 08/03.cpp
+++ 08/03.cpp
@@ -22,7 +22,8 @@ were found in that bin range. Do this for each color channel, BGR.
 #include <opencv2/imgproc.hpp>
 
 using namespace cv;
-using std::cout; using std::endl; using std::string; using std::vector;
+using std::cout; using std::endl; using std::string; using std::vector; using std::ostream;
+using std::to_string;
 
 class HistogramViewer
 {
@@ -42,11 +43,14 @@ private:
     Rect mouse_drag_region;
     string filename;
     string image_window_name = "Image";
+    string graph_window_name = "Color Histogram";
     
     histogram_bins calculate_bins(const Mat&);
     void show_histogram();
+    void draw_graph(const histogram_bins&);
     void on_mouse_pressed(int event, int x, int y, int flags, void *userdata);
     void on_mouse_released(int event, int x, int y, int flags, void *userdata);
+    void debug_output(ostream&, const histogram_bins&);
 };
 
 HistogramViewer::HistogramViewer(string filename) :
@@ -54,10 +58,13 @@ HistogramViewer::HistogramViewer(string filename) :
 {
     namedWindow(image_window_name);
     setMouseCallback(image_window_name, HistogramViewer::on_mouse_event, this);
+    
+    namedWindow(graph_window_name);
 }
 
 HistogramViewer::~HistogramViewer()
 {
+    destroyWindow(graph_window_name);
     destroyWindow(image_window_name);
 }
 
@@ -80,15 +87,83 @@ void HistogramViewer::show_histogram()
     
     auto bins = calculate_bins(color_histogram_region);
     
-    int i = 0;
+    //debug_output(cout, bins);
+    draw_graph(bins);
+}
+
+void HistogramViewer::draw_graph(const histogram_bins& bins)
+{
+    // TODO: normalize heights
     
+    // height: 25 + maximum in any bin
+    // width: 20 * 8 bins
+    // type: 3 channel byte
+    Mat graph = Mat::zeros(500, 500, CV_8UC(3));
+    vector<Scalar> bar_colors = { Scalar(0, 0, 200), Scalar(0, 200, 0), Scalar(200, 0, 0)};
+    
+    auto total_pixels = 0;
+    for (auto pixel_count : bins[0])
+    {
+        total_pixels += pixel_count;
+    }
+    
+    for (auto i = 0; i < 3; ++i)
+    {
+        const auto& current_channel = bins[i];
+        const auto& current_bar_color = bar_colors[i];
+        int offset_x = 0 + (5 * i);
+        
+        for (auto j = 0; j < 8; ++j)
+        {
+            const auto& bin_value = current_channel[j];
+            offset_x += 50;
+            //cout << "drawing @ " << offset_x << " in " << current_bar_color << endl;
+            double bar_scale = (double)bin_value / (double)total_pixels;
+            auto bar_height = 300 * bar_scale;
+            cout << bar_height << endl;
+            
+            line(graph, Point(offset_x, 400 - bar_height), Point(offset_x, 400), current_bar_color, 3);
+        }
+        
+    }
+    
+    for (auto i = 0; i < 8; ++i)
+    {
+        string bin_range;
+        bin_range += to_string(i*32);
+        bin_range += "-";
+        bin_range += to_string((i+1)*32-1);
+        putText(
+            graph, 
+            bin_range, 
+            Point(25 + 50 * i, 450 + (i%2 * 25)), // Upper-left corner of text box
+            FONT_HERSHEY_PLAIN, // Font (e.g., cv::FONT_HERSHEY_PLAIN)
+            0.9, // size (a multiplier, not "points"!)
+            Scalar(175, 25, 225), // Color, RGB form
+            1);
+    }
+    
+    imshow(graph_window_name, graph);
+    
+    // for each color
+    //    set color
+    //    calculate offset
+    //    for each range
+    //        calculate offset
+    //        draw label
+    //        draw bar depending on value in color
+}
+
+void HistogramViewer::debug_output(ostream& os, const histogram_bins& bins)
+{
+    int i = 0;
     for (const auto& color_bin : bins)
     {
-        cout << "channel " << i << endl;
+        os << "channel " << i << endl;
         int j = 0;
         for (const auto& value : color_bin)
         {
-            cout << j*32 << "-" << (j+1)*32-1 << ": " << value << endl;
+            os << j*32 << "-" << (j+1)*32-1 << ": " << value << endl;
             ++j;
         }
         ++i;
