4ef172d63725a770188ddfb434af861aa7dc3add
Zeller Christoph <chze7458@ESO9573-ubuntu.eso.local>
2018-09-25 09:45:23 +0200

    added basic histogram data calculation & debug output for 08.03

--- 08/03.cpp
+++ 08/03.cpp
@@ -16,12 +16,13 @@ were found in that bin range. Do this for each color channel, BGR.
 
 #include <iostream>
 #include <string>
+#include <vector>
 #include <opencv2/core.hpp>
 #include <opencv2/highgui.hpp>
 #include <opencv2/imgproc.hpp>
 
 using namespace cv;
-using std::cout; using std::endl; using std::string;
+using std::cout; using std::endl; using std::string; using std::vector;
 
 class HistogramViewer
 {
@@ -32,13 +33,17 @@ public:
     void show();
     static void on_mouse_event(int event, int x, int y, int flags, void *userdata);
     
+    typedef vector<vector<int>> histogram_bins;
+    
 private:
     Mat image;
     Mat display_image;
-    Mat color_histogram_region;
-    string filename;
     
-    void calculate_bins();
+    Rect mouse_drag_region;
+    string filename;
+    string image_window_name = "Image";
+    
+    histogram_bins calculate_bins(const Mat&);
     void show_histogram();
     void on_mouse_pressed(int event, int x, int y, int flags, void *userdata);
     void on_mouse_released(int event, int x, int y, int flags, void *userdata);
@@ -47,34 +52,93 @@ private:
 HistogramViewer::HistogramViewer(string filename) :
  filename(filename)
 {
-    namedWindow("Image");
-    setMouseCallback("Image", HistogramViewer::on_mouse_event, this);
+    namedWindow(image_window_name);
+    setMouseCallback(image_window_name, HistogramViewer::on_mouse_event, this);
 }
 
 HistogramViewer::~HistogramViewer()
 {
-    destroyWindow("Image");
+    destroyWindow(image_window_name);
 }
 
 void HistogramViewer::show()
 {
     image = imread(filename);
     display_image = image.clone();
-    color_histogram_region;
+    //color_histogram_region;
     
-    imshow("Image", image);
+    imshow(image_window_name, display_image);
     
     waitKey(0);
 }
 
+void HistogramViewer::show_histogram()
+{
+    Mat color_histogram_region (image, mouse_drag_region);
+    rectangle(display_image, mouse_drag_region, Scalar(175, 25, 220));
+    imshow(image_window_name, display_image);
+    
+    auto bins = calculate_bins(color_histogram_region);
+    
+    int i = 0;
+    
+    for (const auto& color_bin : bins)
+    {
+        cout << "channel " << i << endl;
+        int j = 0;
+        for (const auto& value : color_bin)
+        {
+            cout << j*32 << "-" << (j+1)*32-1 << ": " << value << endl;
+            ++j;
+        }
+        ++i;
+    }
+}
+
+HistogramViewer::histogram_bins HistogramViewer::calculate_bins(const Mat& region)
+{
+    // TODO: get rid of magic numbers for channels, bin width & amount
+    HistogramViewer::histogram_bins bins;
+    bins.push_back(vector<int> (8, 0));
+    bins.push_back(vector<int> (8, 0));
+    bins.push_back(vector<int> (8, 0));
+    
+    for (MatConstIterator_<Vec3b> it = region.begin<Vec3b>(); it != region.end<Vec3b>(); ++it)
+    {
+        //cout << *it << endl;
+        Vec3b v = *it;
+        auto b = v[0], g = v[1], r = v[2];
+        
+        auto b_bin = b / 32;
+        auto g_bin = g / 32;
+        auto r_bin = r / 32;
+        
+        bins[0][b_bin]++;
+        bins[1][g_bin]++;
+        bins[2][r_bin]++;
+    }
+    
+    return bins;
+}
+
 void HistogramViewer::on_mouse_pressed(int event, int x, int y, int flags, void *userdata)
 {
-    cout << "button down fired..." << endl;
+    image.copyTo(display_image);
+    //cout << "button down fired..." << endl;
+    mouse_drag_region.x = x;
+    mouse_drag_region.y = y;
 }
 
 void HistogramViewer::on_mouse_released(int event, int x, int y, int flags, void *userdata)
 {    
-    cout << "button up fired..." << endl;
+    //cout << "button up fired..." << endl;
+    // TODO: this only supports topLeft->bottomRight dragging for now. exceptions! yay!
+    mouse_drag_region.height = y - mouse_drag_region.y;
+    mouse_drag_region.width = x - mouse_drag_region.x;
+    
+    cout << mouse_drag_region << endl;
+    
+    show_histogram();
 }
 
 void HistogramViewer::on_mouse_event(int event, int x, int y, int flags, void *userdata)
